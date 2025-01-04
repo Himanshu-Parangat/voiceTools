22e4b50dba2e982c3ebbd74e1b846c368643d083
Himanshu-Parangat <himanshuparangat005@gmail.com>
2025-01-04 23:53:40 +0530

    initial server run

--- run.py
+++ run.py
@@ -3,10 +3,13 @@
 this will start the main application
 """
 
-from src.app import main
+import uvicorn
+from src.app import main,app 
 
 print("Hihi, from the voiceTool devs\n\n")
 
 
 if __name__ == "__main__":
     main()
+    uvicorn.run(app)
+    
