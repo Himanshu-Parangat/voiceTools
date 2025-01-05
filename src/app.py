e6f8ec6ece768f92d8bfe291afb5a30ec30c0b44
Himanshu-Parangat <himanshuparangat005@gmail.com>
2025-01-05 21:47:56 +0530

    render index template

--- src/app.py
+++ src/app.py
@@ -1,18 +1,18 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 def main():
     print("project running...")
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="src/component")
 
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/dashboard")
+async def dashboard(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
