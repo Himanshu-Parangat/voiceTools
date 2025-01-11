887017bcccfc5e98a441cafdc32ba0db78641133
Himanshu-Parangat <himanshuparangat005@gmail.com>
2025-01-11 19:57:13 +0530

    onboarding

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 import uvicorn
 from src.configuration.config_handler import handle_config
 
@@ -8,16 +9,23 @@ templates = Jinja2Templates(directory="src/component")
 
 
 @app.get("/setup")
-async def setup():
-    return {"allow": "permission", "type-mic":"mic","type-audio":"audio"}
+async def setup(request: Request):
+    return templates.TemplateResponse("onboarding.html", {"request": request})
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def root():
+    if check_onboarding_status():
+        return RedirectResponse(url="/setup")
+    else:
+        return RedirectResponse(url="/dashboard")
 
 @app.get("/dashboard")
 async def dashboard(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+def check_onboarding_status():
+    return True
+
 
 def run_app():
     print("\napplication is running...")
